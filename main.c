7f6c5dcf63e2876c9b8d7bddc92ff36fe3ed418a
Arhiiiip <rabokony@icloud.com>
2024-10-11 12:01:25 +0300

    one more try...

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#define READ 0
+#define WRITE 1
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,22 +13,22 @@
 
 const char* log_pipe_open_fmt = "Pipe (%d; %d) created. read_fd=%d, write_fd=%d\n";
 
-typedef struct fd_pair {
+typedef struct pipe {
     int fd[2];
-} fd_pair;
+} pipe;
 
-fd_pair** pipes;
+pipe** pipes;
 local_id X;
 local_id p_id = 0;
 
 FILE* events_log_file;
 FILE* pipes_log_file;
 
-void task_child(void);
-void task_parent(void);
+void task_isChild(void);
+void task_isParent(void);
 
 int send(void* self, local_id dst, const Message* msg) {
-    long bytes_written = write(((fd_pair*) self)[dst].fd[1], msg, sizeof(MessageHeader) + msg->s_header.s_payload_len);
+    long bytes_written = write(((pipe*) self)[dst].fd[WRITE], msg, sizeof(MessageHeader) + msg->s_header.s_payload_len);
     return bytes_written != (sizeof(MessageHeader) + msg->s_header.s_payload_len);
 }
 
@@ -34,7 +37,7 @@ int send_multicast(void* self, const Message* msg) {
     fprintf(events_log_file, "%s", msg->s_payload);
     for (local_id i = 0; i <= X; i++) {
         if (i != p_id) {
-            if (send(((fd_pair**) self)[p_id], i, msg) != 0) {
+            if (send(((pipe**) self)[p_id], i, msg) != 0) {
                 return -1;
             }
         }
@@ -43,8 +46,8 @@ int send_multicast(void* self, const Message* msg) {
 }
 
 int receive(void* self, local_id from, Message* msg) {
-    long bytes_read = read(((fd_pair*) self)[from].fd[0], msg, sizeof(MessageHeader));
-    bytes_read += read(((fd_pair*) self)[from].fd[0], &msg->s_payload, msg->s_header.s_payload_len);
+    long bytes_read = read(((pipe*) self)[from].fd[READ], msg, sizeof(MessageHeader));
+    bytes_read += read(((pipe*) self)[from].fd[READ], &msg->s_payload, msg->s_header.s_payload_len);
     return bytes_read <= 0;
 }
 
@@ -52,7 +55,7 @@ int receive_all(void* self, Message* msg) {
     for (local_id i = 1; i <= X; i++) {
         if (i != p_id) {
             memset(msg, 0, MAX_MESSAGE_LEN);
-            if (receive(((fd_pair**) self)[i], p_id, msg) != 0) {
+            if (receive(((pipe**) self)[i], p_id, msg) != 0) {
                 return -1;
             }
         }
@@ -71,7 +74,7 @@ void create_pipes(void) {
         for (local_id j = 0; j <= X; j++) {
             if (i != j) {
                 pipe(pipes[i][j].fd);
-                fprintf(pipes_log_file, log_pipe_open_fmt, i, j, pipes[i][j].fd[0], pipes[i][j].fd[1]);
+                fprintf(pipes_log_file, log_pipe_open_fmt, i, j, pipes[i][j].fd[READ], pipes[i][j].fd[WRITE]);
             }
         }
     }
@@ -81,13 +84,13 @@ void close_unused_pipes(void) {
     for (local_id i = 0; i <= X; i++) {
         for (local_id j = 0; j <= X; j++) {
             if (i != p_id && j != p_id && i != j) {
-                close(pipes[i][j].fd[0]);
-                close(pipes[i][j].fd[1]);
+                close(pipes[i][j].fd[READ]);
+                close(pipes[i][j].fd[WRITE]);
             }
         }
         if (i != p_id) {
-            close(pipes[p_id][i].fd[0]);
-            close(pipes[i][p_id].fd[1]);
+            close(pipes[p_id][i].fd[READ]);
+            close(pipes[i][p_id].fd[WRITE]);
         }
     }
 }
@@ -95,8 +98,8 @@ void close_unused_pipes(void) {
 void close_pipes(void) {
     for (local_id i = 0; i <= X; i++) {
         if (i != p_id) {
-            close(pipes[p_id][i].fd[1]);
-            close(pipes[i][p_id].fd[0]);
+            close(pipes[p_id][i].fd[WRITE]);
+            close(pipes[i][p_id].fd[READ]);
         }
         free(pipes[i]);
     }
@@ -109,25 +112,27 @@ void init_message_header(Message* msg, MessageType type) {
     msg->s_header.s_type = type;
 }
 
-void task_child(void) {
-    close_unused_pipes();
-    Message* msg = malloc(MAX_MESSAGE_LEN);
-
-    init_message_header(msg, STARTED);
-    sprintf(msg->s_payload, log_started_fmt, p_id, getpid(), getppid());
+void spread(Message* msg, MessageType type) {
+    init_message_header(msg, type);
+    if(type == STARTED) {
+        sprintf(msg->s_payload, log_started_fmt, p_id, getpid(), getppid());
+    }
+    if(type == DONE) {
+        sprintf(msg->s_payload, log_done_fmt, p_id);
+    }
     msg->s_header.s_payload_len = strlen(msg->s_payload);
     send_multicast(pipes, msg);
+}
 
+void task_isChild(void) {
+    close_unused_pipes();
+    Message* msg = malloc(MAX_MESSAGE_LEN);
+    spread(msg, STARTED);
     receive_all(pipes, msg);
     printf(log_received_all_started_fmt, p_id);
     fprintf(events_log_file, log_received_all_started_fmt, p_id);
     memset(msg, 0, MAX_MESSAGE_LEN);
-
-    init_message_header(msg, DONE);
-    sprintf(msg->s_payload, log_done_fmt, p_id);
-    msg->s_header.s_payload_len = strlen(msg->s_payload);
-    send_multicast(pipes, msg);
-
+    spread(msg, DONE);
     receive_all(pipes, msg);
     printf(log_received_all_done_fmt, p_id);
     fprintf(events_log_file, log_received_all_done_fmt, p_id);
@@ -136,7 +141,7 @@ void task_child(void) {
     close_pipes();
 }
 
-void task_parent(void) {
+void task_isParent(void) {
     close_unused_pipes();
     Message* msg = malloc(MAX_MESSAGE_LEN);
 
@@ -149,25 +154,21 @@ void task_parent(void) {
 }
 
 int main(int argc, char** argv) {
+    events_log_file = fopen(events_log, "w");
+    pipes_log_file = fopen(pipes_log, "w");
     if (argc == 3 && strcmp(argv[1], "-p") == 0) {
         X = (local_id) atoi(argv[2]);
         if (X > 0 && X < 11) {
-            events_log_file = fopen(events_log, "w");
-            pipes_log_file = fopen(pipes_log, "w");
-
             create_pipes();
 
             for (local_id i = 1; i <= X; i++) {
                 if (fork() == 0) {
                     p_id = i;
-                    task_child();
+                    task_isChild();
                     return 0;
                 }
             }
-            task_parent();
-
-            fclose(pipes_log_file);
-            fclose(events_log_file);
+            task_isParent();
         } else {
             printf("Value must be in range [1..10]!");
             return 1;
@@ -176,5 +177,7 @@ int main(int argc, char** argv) {
         printf("Usage: -p <value>");
         return 1;
     }
+    fclose(pipes_log_file);
+    fclose(events_log_file);
     return 0;
 }
